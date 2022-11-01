22a23dfc00c5ce561344702d13b6c0ce57cca405
shaialoni <alonishai@gmail.com>
2022-11-01 15:48:39 -0700

    set up display of google maps

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,13 @@ console.log(destination)
 
 const cargo = new Cargo()
 console.log(cargo)
+
+
+
+new google.maps.Map(document.getElementById('g-map'), {
+    zoom: 1.6,
+    center: {
+        lat: 0,
+        lng: 20,
+    }
+})
